50e685668a143b9dd2fb3ab0d4c4b7cbee23fe24
seito-developer <seito.horiguchi@gmail.com>
2023-05-08 03:40:53 +0900

    added useLocation

--- laravel/resources/js/components/BoardList.jsx
+++ laravel/resources/js/components/BoardList.jsx
@@ -22,7 +22,9 @@ const BoardList = () => {
     if(boards){
       return boards.map(board => {
         return (
-          <Link to={"/boards"} key={board.id} href="#" className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+          <Link to={"/boards"} 
+            state={{ boardId: board.id }}
+            key={board.id} href="#" className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
               <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
                 {board.title}
               </h5>
--- laravel/resources/js/pages/Boards.jsx
+++ laravel/resources/js/pages/Boards.jsx
@@ -1,12 +1,19 @@
 import React, { useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
 
 
 const Boards = () => {
+    const { state } = useLocation()
+    const [boardId, setBoardId] = useState([])
+
+    useEffect(() => {
+        setBoardId(state.boardId)
+    }, [])
     
     return (
         <div>
             <h1>Boards</h1>
-            
+            {boardId}
         </div>
     );
 }
