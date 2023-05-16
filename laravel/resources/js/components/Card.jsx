edaf7ef7405e7f3825b96939444c22216a3b5f14
seito-developer <seito.horiguchi@gmail.com>
2023-05-16 15:51:20 +0900

    cards OK

--- laravel/resources/js/components/Card.jsx
+++ laravel/resources/js/components/Card.jsx
@@ -1,16 +1,14 @@
-import React, { Component, useEffect, useState } from "react";
-import { getCards } from "../api";
+import React, { useEffect, useState } from "react";
 
 const Card = (props) => {
     const [cards, setCards] = useState([]);
 
     useEffect(() => {
-      console.log('props.cardData:', props.cardData);
         const listCards = props.cardData.filter(
             (card) => card.list_id === props.tasklistId
         );
         setCards(listCards);
-    }, []);
+    }, [props.cardData]);
 
     return (
         <div>
--- laravel/resources/js/components/Tasklist/Tasklist.jsx
+++ laravel/resources/js/components/Tasklist/Tasklist.jsx
@@ -26,6 +26,7 @@ const Tasklist = (props) => {
 
     const handleCardAdded = (newCard) => {
         setAllCards([...allCards, newCard]);
+        fetchCards();
     };
 
     const renderTasklists = () => {
