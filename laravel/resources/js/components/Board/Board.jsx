8e0ecc64d133de033fa7573d404da1a550187839
seito-developer <seito.horiguchi@gmail.com>
2023-05-16 16:17:41 +0900

    made css

--- laravel/resources/js/components/Board/Board.jsx
+++ laravel/resources/js/components/Board/Board.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, Component } from 'react';
-import { getBoards } from '../api';
+import { getBoards } from '../../api';
 import { Link } from 'react-router-dom';
+import "./Board.scss";
 
 const Board = (props) => {
 
@@ -11,10 +12,10 @@ const Board = (props) => {
         return (
           <Link to={"/board"}
             state={{ boardId: item.id }}
-            key={item.id} href="#" className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-              <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+            key={item.id} href="#" className="board">
+              <h2 className="board__title">
                 {item.title}
-              </h5>
+              </h2>
               {/* <p className="font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p> */}
           </Link>
         )
--- laravel/resources/js/pages/Home/Home.jsx
+++ laravel/resources/js/pages/Home/Home.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import Board from "../../components/Board";
+import Board from "../../components/Board/Board";
 import { getBoards } from "../../api";
 import AddBoard from "../../components/AddBoard";
 import Layout from "../../components/Layout/Layout";
